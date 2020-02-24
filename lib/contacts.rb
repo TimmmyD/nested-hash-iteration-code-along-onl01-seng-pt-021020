8258ba021245b85fa10c3a657b089ea79b09c976
Tim Donoghue <tmd112593@gmail.com>
2020-02-24 00:30:52 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,13 +1,11 @@
-
-  
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_has.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
           data.delete_if {|ice_cream| ice_cream == "strawberry"}
-    end
-  end
-end 
-
+        end
+      end
+    end 
+  end 
 end
